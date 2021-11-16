8c0b26b9027a203e37119912c5f0daf421590d61
Aleksey Likhodievski <lesha.liho@gmail.com>
2021-11-16 21:11:45 +0300

    feat: work functionized

--- homeworks/hw1/index.js
+++ homeworks/hw1/index.js
@@ -1,17 +1,34 @@
-const [surname, userName, patronymic] = prompt('Enter your credentials').split(
-  ' ',
-);
-const age = Number(prompt('Enter your age'));
-const sex = prompt('Enter your sex');
+const showInfo = ({ sex, surname, userName, patronymic, age, retired }) => {
+  alert(
+    `Hi, ${
+      sex === 'male' ? 'Mr. ' : 'Ms. '
+    } ${surname}, your name is ${userName} and patronymic ${patronymic}, your age in ${age}, it is ${
+      age * 365
+    } days, ${
+      retired ? 'you are retired' : 'you are fresh and hungry for work'
+    }, five years till now you will be ${age + 5} yo`,
+  );
+};
 
-alert(
-  `Hi, ${
-    sex === 'male' ? 'Mr' : 'Mrs'
-  } ${surname}, your name is ${userName} and patronymic ${patronymic}, your age in ${age}, it is ${
-    age * 365
-  } days, ${
-    (sex === 'female' && age >= 55) || (sex === 'male' && age >= 65)
-      ? 'you are retired'
-      : 'you are fresh and hungry for work'
-  }, five years till now you will be ${age + 5} yo`,
-);
+const isRetired = ({ sex, age }) => {
+  if ((sex === 'female' && age >= 55) || (sex === 'male' && age >= 65))
+    return true;
+  else return false;
+};
+
+const init = () => {
+  const [surname, userName, patronymic] = prompt(
+    'Enter your credentials',
+  ).split(' ');
+  let age = Number(prompt('Enter your age'));
+  while (isNaN(age)) {
+    age = Number(prompt('Enter your actual age'));
+  }
+  const sex = prompt('Enter your sex');
+
+  const retired = isRetired({ sex, age });
+
+  showInfo({ sex, surname, userName, patronymic, age, retired });
+};
+
+init();
