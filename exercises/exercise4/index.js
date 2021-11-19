58d2b94fb646564110a4ca303c1df69f75471677
Aleksey Likhodievski <lesha.liho@gmail.com>
2021-11-19 16:37:08 +0300

    fix: now exercise 4 is complite

---
+++ exercises/exercise4/index.js
@@ -0,0 +1,8 @@
+const div = document.getElementById('less');
+const header = document.querySelector('h1');
+const qsAll = div.querySelectorAll('p');
+const qs = div.querySelector('.qs');
+
+console.log(div);
+console.log(qs);
+console.log([...qsAll]);
